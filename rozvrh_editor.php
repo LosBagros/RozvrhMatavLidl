e7e11a3844e0de375e1bbd6b0835591a46f4f65f
Bagros <wifi5@centrum.cz>
2023-04-26 00:23:44 +0200

    rozvrh editor

--- rozvrh_editor.php
+++ rozvrh_editor.php
@@ -19,11 +19,42 @@ $sql = "SELECT * FROM casove_useky";
 $result = $conn->query($sql);
 $casove_useky = $result->fetch_all(MYSQLI_ASSOC);
 
+//$sql = "SELECT DISTINCT den_v_tydnu, casovy_usek_id, zkratka_predmetu, nazev_predmetu, zkratka_vyucujiciho, jmeno_vyucujiciho, ucebna FROM rozvrh";
+$sql = "SELECT DISTINCT den_v_tydnu FROM rozvrh";
+$result = $conn->query($sql);
+$dny_v_tydnu = $result->fetch_all(MYSQLI_ASSOC);
+
+$sql = "SELECT DISTINCT casovy_usek_id FROM rozvrh";
+$result = $conn->query($sql);
+$casove_useky = $result->fetch_all(MYSQLI_ASSOC);
+
+$sql = "SELECT DISTINCT zkratka_predmetu FROM rozvrh";
+$result = $conn->query($sql);
+$zkratky_predmetu = $result->fetch_all(MYSQLI_ASSOC);
+
+$sql = "SELECT DISTINCT nazev_predmetu FROM rozvrh";
+$result = $conn->query($sql);
+$nazvy_predmetu = $result->fetch_all(MYSQLI_ASSOC);
+
+$sql = "SELECT DISTINCT zkratka_vyucujiciho FROM rozvrh";
+$result = $conn->query($sql);
+$zkratky_vyucujicich = $result->fetch_all(MYSQLI_ASSOC);
+
+$sql = "SELECT DISTINCT jmeno_vyucujiciho FROM rozvrh";
+$result = $conn->query($sql);
+$jmena_vyucujicich = $result->fetch_all(MYSQLI_ASSOC);
+
+$sql = "SELECT DISTINCT ucebna FROM rozvrh";
+$result = $conn->query($sql);
+$ucebny = $result->fetch_all(MYSQLI_ASSOC);
+
+
 $conn->close();
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -31,46 +62,98 @@ $conn->close();
     <title>Rozvrh editor | MatavRozvrh</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
+
 <body data-bs-theme="dark">
     <div class="container">
         <h1 class="text-center mt-3">Editor rozvrhu</h1>
         <form method="POST" action="server.php">
             <div class="form-group">
                 <label for="den_v_tydnu">Den v týdnu</label>
-                <input type="number" class="form-control" id="den_v_tydnu" name="den_v_tydnu" min="1" max="7" required>
-            </div>
-            <div class="form-group">
-                <label for="casovy_usek_id">Časový úsek</label>
-                <select class="form-control" id="casovy_usek_id" name="casovy_usek_id" required>
-                    <?php foreach ($casove_useky as $usek): ?>
-                        <option value="<?php echo $usek['id']; ?>"><?php echo $usek['zacatek'] . ' - ' . $usek['konec']; ?></option>
-                    <?php endforeach; ?>
+                <select class="form-control" id="den_v_tydnu" name="den_v_tydnu">
+                    <?php 
+                    foreach ($dny_v_tydnu as $den_v_tydnu) {
+                        echo '<option value="' . $den_v_tydnu['den_v_tydnu'] . '">' . $den_v_tydnu['den_v_tydnu'] . '</option>';
+                    }
+                    ?>
                 </select>
-            </div>
-            <div class="form-group">
+
+
+                <label for="casovy_usek_id">Časový úsek</label>
+                <select class="form-control" id="casovy_usek_id" name="casovy_usek_id">
+                    <?php 
+                    foreach ($casove_useky as $casovy_usek) {
+                        echo '<option value="' . $casovy_usek['casovy_usek_id'] . '">' . $casovy_usek['casovy_usek_id'] . '</option>';
+                    }
+                    ?>
+                    
+                </select>
+
                 <label for="zkratka_predmetu">Zkratka předmětu</label>
-                <input type="text" class="form-control" id="zkratka_predmetu" name="zkratka_predmetu" required>
-            </div>
-            <div class="form-group">
+                <select class="form-control" id="zkratka_predmetu" name="zkratka_predmetu">
+                    <?php 
+                    foreach ($zkratky_predmetu as $zkratka_predmetu) {
+                        echo '<option value="' . $zkratka_predmetu['zkratka_predmetu'] . '">' . $zkratka_predmetu['zkratka_predmetu'] . '</option>';
+                    }
+                    ?>
+                </select>
+
                 <label for="nazev_predmetu">Název předmětu</label>
-                <input type="text" class="form-control" id="nazev_predmetu" name="nazev_predmetu" required>
-            </div>
-            <div class="form-group">
+                <select class="form-control" id="nazev_predmetu" name="nazev_predmetu">
+                    <?php 
+                    foreach ($nazvy_predmetu as $nazev_predmetu) {
+                        echo '<option value="' . $nazev_predmetu['nazev_predmetu'] . '">' . $nazev_predmetu['nazev_predmetu'] . '</option>';
+                    }
+                    ?>
+                </select>
+
                 <label for="zkratka_vyucujiciho">Zkratka vyučujícího</label>
-                <input type="text" class="form-control" id="zkratka_vyucujiciho" name="zkratka_vyucujiciho" required>
-            </div>
-            <div class="form-group">
+                <select class="form-control" id="zkratka_vyucujiciho" name="zkratka_vyucujiciho">
+                    <?php 
+                    foreach ($zkratky_vyucujicich as $zkratka_vyucujiciho) {
+                        echo '<option value="' . $zkratka_vyucujiciho['zkratka_vyucujiciho'] . '">' . $zkratka_vyucujiciho['zkratka_vyucujiciho'] . '</option>';
+                    }
+                    ?>
+                </select>
+
                 <label for="jmeno_vyucujiciho">Jméno vyučujícího</label>
-                <input type="text" class="form-control" id="jmeno_vyucujiciho" name="jmeno_vyucujiciho" required>
-            </div>
-            <div class="form-group">
+                <select class="form-control" id="jmeno_vyucujiciho" name="jmeno_vyucujiciho">
+                    <?php 
+                    foreach ($jmena_vyucujicich as $jmeno_vyucujiciho) {
+                        echo '<option value="' . $jmeno_vyucujiciho['jmeno_vyucujiciho'] . '">' . $jmeno_vyucujiciho['jmeno_vyucujiciho'] . '</option>';
+                    }
+                    ?>
+                </select>
+
                 <label for="ucebna">Učebna</label>
-                <input type="text" class="form-control" id="ucebna" name="ucebna" required>
+                <select class="form-control" id="ucebna" name="ucebna">
+                    <?php 
+                    foreach ($ucebny as $ucebna) {
+                        echo '<option value="' . $ucebna['ucebna'] . '">' . $ucebna['ucebna'] . '</option>';
+                    }
+                    ?>
+                    </option>
+                    <option value="other">Other</option>
+                </select>
+                <div id="ucebna-custom" style="display:none;">
+                    <input type="text" class="form-control mt-2" id="ucebna-custom-input" name="ucebna_custom" placeholder="Název učebny">
+                </div>
             </div>
-            <button type="submit" class="btn btn-primary mt-3" name="update_rozvrh">Uložit změny</button>
         </form>
     </div>
     <script src="./js/jquery-3.6.4.min.js"></script>
+    <script>
+        $(document).ready(function () {
+            $('#ucebna').change(function () {
+                if ($(this).val() == 'other') {
+                    $('#ucebna-custom').show();
+                    $('#ucebna-custom-input').attr('required', true);
+                } else {
+                    $('#ucebna-custom').hide();
+                    $('#ucebna-custom-input').attr('required', false);
+                }
+            });
+        });
+    </script>
 </body>
-</html>
 
+</html>
